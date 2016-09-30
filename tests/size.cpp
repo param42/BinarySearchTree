202c308c7916f9c71e3f529e11a0979c5481b519
param42 <param42@ya.ru>
2016-09-30 16:37:24 +0300

    Update size.cpp

--- tests/size.cpp
+++ tests/size.cpp
@@ -1,1 +1,24 @@
-
+#include "catch.hpp"
+SCENARIO("size off empty tree must be equal"){
+  GIVEN("emptytree"){
+     
+    WHEN ("decide size"){
+       BinarySearchTree<T> tree;
+      THEN("size must be equal")
+        REQUIRE(tree.size()==0);
+    }  
+ 
+  
+      WHEN("not empry tree"){
+        BinarySearchTree<T> tree {234,4234,421};
+        THEN("size must be not equal")
+          REQUIRE(tree.size()!=0);
+  }
+    WHEN("const"){
+      const BinarySearchTree<T> tree {234,4234,421};
+      BinarySearchTree<T> const_tree {234,4234,421};
+      THEN("const and not const")
+        REQUIRE(tree.size()!=0 && const_tree.size()!=0);
+    }
+  }
+}
