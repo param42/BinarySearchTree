ba85b84d419abf5ac0f270c09bddbac24f104c00
param42 <param42@ya.ru>
2016-09-30 16:47:37 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,8 @@
 #include "../include/BinarySearchTree.h"
 #include <string>
 #include <fstream>
-
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
 int main()
 {
 	BinarySearchTree<int> bst1{ 5345, 435, 435, 34, 534 };
