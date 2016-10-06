564f211aa3d92dec3fb67b7f92656284656e7d25
param42 <param42@ya.ru>
2016-10-06 17:10:35 +0300

    Update size.cpp

--- tests/size.cpp
+++ tests/size.cpp
@@ -1,24 +1,49 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "BinarySearchTree.hpp"
 
-SCENARIO( "vectors " ) {
-
-    GIVEN( "A BinaryST with some items" ) {
-        BinarySearchTree <int> test;
-        
-        WHEN("if BST null- size==0")
+SCENARIO("Size of empty tree must be equal 0")
+{
+    GIVEN("Empty tree")
+    {
+        BinarySearchTree<int> tree;
+        WHEN("Decide size")
         {
-          THEN("the size==0")
+            THEN("Size must be equal 0")
             {
-            REQUIRE( test.size() == 0 );
+                REQUIRE(tree.size() == 0);
             }
         }
-        WHEN("if BST not null- size>0")
+    }
+}
+
+SCENARIO("Size of not empty tree must not be equal 0")
+{
+    GIVEN("Not empty tree")
+    {
+        BinarySearchTree<int> tree { 8,10,3,14,6,1,7,13,4 };
+        WHEN("Decide size")
         {
-          const BinarySearchTree<int> test1{1};
-          BinarySearchTree<int> test2{1};
-          THEN("the size>0")
-           {
-            REQUIRE ( test1.size()>0 && test2.size()>0 )
-           }
+            THEN("Size must not be equal 0")
+            {
+                REQUIRE(tree.size() != 0);
+            }
         }
+    }
+}
+
+SCENARIO("size() must support const and non-const objects")
+{
+    GIVEN("const and non-const tree")
+    {
+        const BinarySearchTree<int> tree1 { 8,10,3 };
+        BinarySearchTree<int> tree2 { 7,9,2 };
+        WHEN("Decide size")
+        {
+            THEN("Size must be equal")
+            {
+                REQUIRE(tree1.size() == tree2.size());
+            }
+        }
+    }
+}
+
