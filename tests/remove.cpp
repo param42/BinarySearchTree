2e05b5a7bc16cbaf5aa2183ac36437ca45491307
param42 <param42@ya.ru>
2016-10-13 20:03:57 +0300

    Update remove.cpp

--- tests/remove.cpp
+++ tests/remove.cpp
@@ -10,7 +10,7 @@
            
            WHEN("remove node that hasn't children")
             {
-              bst1.remove(7);
+              bst1.remove(bst1.tree_,7);
               THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
@@ -21,7 +21,7 @@
             bst2 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,30});
              WHEN("remove node that has only right child")
             {
-              bst1.remove(17);
+              bst1.remove(bst1.tree_,17);
               THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
@@ -43,7 +43,7 @@
             bst2 = BinarySearchTree<int> ({ 10,15,14,16,25,7,17,30});
              WHEN("remove node that has both children")
             {
-              bst1.remove(20);
+               bst1.remove(bst1.tree_,20);
               THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
@@ -54,7 +54,7 @@
             bst2 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17,30});
              WHEN("remove node that doesn't exist")
             {
-              bst1.remove(6);
+               bst1.remove(bst1.tree_,6);
               THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
@@ -65,7 +65,7 @@
             bst2 = BinarySearchTree<int> ();
              WHEN("remove root")
             {
-              bst1.remove(10);
+               bst1.remove(bst1.tree_,10);
               THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
