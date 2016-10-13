d95a4e66a99466524cf90880a15583aaa4cf38fe
param42 <param42@ya.ru>
2016-10-13 19:13:32 +0300

    Update remove.cpp

--- tests/remove.cpp
+++ tests/remove.cpp
@@ -1,71 +1,72 @@
 #include <BinarySearchTree.hpp>
 #include "catch.hpp"
 
-  SCENARIO("после удаления элемемнта из дерева структура должна быть другой")
+  SCENARIO("the tree size must decrease by one after removing element")
   {
-      GIVEN("TWO TREES")
-           BinarySearchTree<int> bst1{ 10,15,14,16,20,25,7};
-           BinarySearchTree<int> bst2{ 10,15,14,16,20,25,};
-           WHEN("удаления элемента без дочерних")
+      GIVEN("two trees")
+      {
+           BinarySearchTree<int> bst1{ 10,15,14,16,20,25,7,17,30};
+           BinarySearchTree<int> bst2{ 10,15,14,16,20,25,17,30};
+           
+           WHEN("remove node that hasn't children")
             {
               bst1.remove(7);
-              THEN("деревья должны быть одинаковыми")
+              THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
                 }
              }
              
-             BinarySearchTree<int> bst1{ 10,15,14,16,20,25,7,17};
-             BinarySearchTree<int> bst2{ 10,15,14,16,20,25,7};
-             WHEN("удаления элемента у которого есть один правый ребенок")
+            bst1 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17,30});
+            bst2 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,30});
+             WHEN("remove node that has only right child")
             {
               bst1.remove(17);
-              THEN("деревья должны быть одинаковыми")
+              THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
                 }
              }
              
-             BinarySearchTree<int> bst1{ 10,15,14,16,20,25,7,17,30};
-             BinarySearchTree<int> bst2{ 10,15,14,16,20,25,7,17};
-             WHEN("удаления элемента у которого есть один левый ребенок")
+             bst1 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17,30});
+             bst2 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17});
+             WHEN("remove node that has only right child")
             {
               bst1.remove(30);
-              THEN("деревья должны быть одинаковыми")
+              THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
                 }
              }
              
-             BinarySearchTree<int> bst1{ 10,15,14,16,20,25,7,17,30};
-             BinarySearchTree<int> bst2{ 10,15,14,16,25,7,17,30};
-             WHEN("удаления элемента у которого есть и левый ребенок и правый")
+            bst1 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17,30});
+            bst2 = BinarySearchTree<int> ({ 10,15,14,16,25,7,17,30});
+             WHEN("remove node that has both children")
             {
               bst1.remove(20);
-              THEN("деревья должны быть одинаковыми")
+              THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
                 }
              }
              
-             
-             BinarySearchTree<int> bst1{ 10};
-             BinarySearchTree<int> bst2();
-             WHEN("удаления корня")
+            bst1 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17,30});
+            bst2 = BinarySearchTree<int> ({ 10,15,14,16,20,25,7,17,30});
+             WHEN("remove node that doesn't exist")
+            {
+              bst1.remove(6);
+              THEN("trees must be equal")
+                {
+                  REQUIRE(bst1==bst2);
+                }
+             }
+        
+            bst1 = BinarySearchTree<int> ({ 10});
+            bst2 = BinarySearchTree<int> ();
+             WHEN("remove root")
             {
               bst1.remove(10);
-              THEN("деревья должны быть одинаковыми")
-                {
-                  REQUIRE(bst1==bst2);
-                }
-             }
-             
-             BinarySearchTree<int> bst1{ 10,15,14,16,20,25,7,17,30};
-             BinarySearchTree<int> bst2{ 10,15,14,16,20,25,7,17,30};
-             WHEN("удаление элемента, которого нет в дереве")
-            {
-              bst1.remove(6666);
-              THEN("деревья должны быть одинаковыми")
+              THEN("trees must be equal")
                 {
                   REQUIRE(bst1==bst2);
                 }
