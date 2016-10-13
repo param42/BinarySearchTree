ec6ee0a1fd2722bf672d5cfe4310aedd360767c1
param42 <param42@ya.ru>
2016-10-13 19:05:25 +0300

    Update BinarySearchTree.hpp

--- include/BinarySearchTree.hpp
+++ include/BinarySearchTree.hpp
@@ -1,20 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <memory>
+
+
 
 template<typename T>
 class BinarySearchTree;
 
-/*
-операртор перемещения+, копирования, сравнения
->> istram
-конструкторы перемещения, копироваания 
-
-добавить симметричный и исполььовать его в одно из операторов +
-
-o
-is
-of
-
-732
-*/
 template <typename T>
 std::ofstream & operator << (std::ofstream & out, const BinarySearchTree<T> & tree)
 {
@@ -41,51 +34,52 @@ template<typename T>
 class BinarySearchTree
 {
 public:
-	class  Node{
+	class  Node {
 	public:
 		T value_;
-	 
-		
+
+
 		std::shared_ptr<Node> left_;
 		std::shared_ptr<Node> right_;
 		std::shared_ptr<Node> parent_;
-		Node() : left_(nullptr), right_(nullptr),parent_(nullptr) {}
+		Node() : left_(nullptr), right_(nullptr), parent_(nullptr) {}
 
-		Node(T value) : value_(value), left_(nullptr), right_(nullptr),parent_(nullptr) {};
+		Node(T value) : value_(value), left_(nullptr), right_(nullptr), parent_(nullptr) {};
 
-		Node(T value, std::shared_ptr<Node>parenttt) : value_(value), left_(nullptr), right_(nullptr),parent_(parenttt) {};
+		Node(T value, std::shared_ptr<Node>parenttt) : value_(value), left_(nullptr), right_(nullptr), parent_(parenttt) {};
 
-		std::shared_ptr<Node> copy(){
+		std::shared_ptr<Node> copy() {
 
-			 
+
 			std::shared_ptr<Node> nNode = std::make_shared<Node>(value_);
 			//auto nNode = std::make_shared<Node>(value_);
 			//Node* nNode = new Node(value_);
 			//std::shared_ptr<int> x_ptr(new int(42));
-			if (left_){
+			if (left_) {
 				nNode->left_ = left_->copy();
 			}
-			if (right_){
+			if (right_) {
 				nNode->right_ = right_->copy();
 			}
 			return nNode;
 		}
 
-		
+
 		~Node()
 		{
+		 
 			left_ = nullptr;
 			right_ = nullptr;
 			parent_ = nullptr;
 		}
-		
+
 	};
 	std::shared_ptr<Node> root_;
 	/*Node* root_;*/
 	size_t size_;
 
 	BinarySearchTree() : root_(nullptr), size_(0) {};
-	
+
 	BinarySearchTree(const std::initializer_list<T> & list)
 	{
 		size_ = 0;
@@ -97,13 +91,13 @@ public:
 
 	};
 
-	BinarySearchTree(BinarySearchTree&& rtree){
+	BinarySearchTree(BinarySearchTree&& rtree) {
 		size_ = rtree.size_;
 		root_ = rtree.root_;
 		rtree.root_ = nullptr;
 		rtree.size_ = 0;
 	}
-	BinarySearchTree(const BinarySearchTree& tree){	
+	BinarySearchTree(const BinarySearchTree& tree) {
 		size_ = tree.size_;
 		root_ = tree.root_->copy();
 	}
@@ -113,7 +107,7 @@ public:
 		return root_;
 	}
 
-	
+
 	~BinarySearchTree()
 	{
 		//delete root_;
@@ -138,7 +132,7 @@ public:
 
 		if (!root_)
 		{
-			
+
 			root_ = std::make_shared<Node>(value);
 			size_++;
 			return true;
@@ -154,21 +148,21 @@ public:
 			else if (value < my_node->value_)
 			{
 				this_node = my_node->left_;
-				 
+
 			}
 			else if (value > my_node->value_)
 			{
 				this_node = my_node->right_;
-			 
+
 			}
 		}
 		if (value < my_node->value_)
 		{
-			my_node->left_ = std::make_shared<Node>(value,my_node);
+			my_node->left_ = std::make_shared<Node>(value, my_node);
 		}
 		else
 		{
-			my_node->right_ = std::make_shared<Node>(value,my_node);
+			my_node->right_ = std::make_shared<Node>(value, my_node);
 		};
 		size_++;
 		return true;
@@ -200,92 +194,122 @@ public:
 		return nullptr;
 	};
 
-	BinarySearchTree<T>& operator = (const BinarySearchTree<T> & tree){//присваивания
-		 root_=nullptr;
+	BinarySearchTree<T>& operator = (const BinarySearchTree<T> & tree) {//присваивания
+		root_ = nullptr;
 		this->root_ = tree.root_->copy();
 		this->size_ = tree.size_;
 		return *this;// ?
 	}
-	BinarySearchTree<T>& operator = (BinarySearchTree<T> && tree){//оператор присваивания для rvalue  move перемещения
-		if (this != &tree){
+	BinarySearchTree<T>& operator = (BinarySearchTree<T> && tree) {//оператор присваивания для rvalue  move перемещения
+		if (this != &tree) {
 			size_ = std::move(tree.size_);
 			root_ = std::move(tree.root_);
 			tree.size_ = 0;
 			tree.root_ = nullptr;
 		}
-		
+
 		return *this;
 	}
-	bool operator == (const BinarySearchTree<T> & tree){
+	bool operator == (const BinarySearchTree<T> & tree) {
+	
 		if (this == &tree) return true;
 
-		return srav(root_,tree.root_);
+		return srav(root_, tree.root_);
 	}
 
-	bool srav(std::shared_ptr<Node> Fnode, std::shared_ptr<Node> Snode){
-		if (Fnode && Snode){
+	bool srav(std::shared_ptr<Node> Fnode, std::shared_ptr<Node> Snode) {
+		if (Fnode!=nullptr && Snode !=nullptr) {
 			return (Fnode->value_ == Snode->value_ && srav(Fnode->left_, Snode->left_) && srav(Fnode->right_, Snode->right_));
 		}
-		else return false;
+		if ((Fnode != nullptr && Snode == nullptr) || (Fnode == nullptr && Snode != nullptr)) {
+			return false;
+		}
+		return true;
 	}
 
-	
 
-	void DeleteNodeFromBinary(std::shared_ptr<Node> node, int value)
+
+	void remove(std::shared_ptr<Node> node, int value)
 	{
-		 
-		if (node == nullptr)//1
+
+		if (node == nullptr)
 			return;
 
 		if (value < node->value_)
-			return DeleteNodeFromBinary(node->left_, value);
+			return remove(node->left_, value);
 		else if (value > node->value_)
-			return DeleteNodeFromBinary(node->right_, value);
-		else  {
+			return remove(node->right_, value);
+		else {
+
+			if (node == root_) {
+				std::shared_ptr<Node> newnode=Leftmost(node->right_);
+				
+				if (Leftmost(node->right_) == node->right_) {
+					newnode->right_ = node->right_->right_;
+				}
+				else {//b
+					newnode->right_ = node->right_;
+					Leftmost(node->right_)->parent_->left_ = nullptr;
+				}
+				root_ = newnode;
+				newnode->parent_ = nullptr;
+				node = nullptr;
+				return;
+			}
 
 			if (node->left_ == nullptr && node->right_ == nullptr) {
 				if (node->parent_->left_ == node)
 					node->parent_->left_ = nullptr;
-				else                             
+				else
 					node->parent_->right_ = nullptr;
-				 node=nullptr;//2
+				node = nullptr;//2
 			}
 			else {
 				std::shared_ptr<Node> newnode;
-				if(node->left_ && node->right_)    
-					{
+				if (node->left_ && node->right_)
+				{
 					newnode = Leftmost(node->right_);
+					Leftmost(node->right_)->parent_->left_ = Leftmost(node->right_)->right_;
 					newnode->parent_ = node->parent_;
-
-					if (Leftmost(node->right_) == node->right_){
+					
+					if (newnode == node->right_) {
 						newnode->right_ = node->right_->right_;
 					}
-					else{//b
+					else {//b
 						newnode->right_ = node->right_;
+						 
 					}
 					newnode->left_ = node->left_;
-					
-					
-					if (node->parent_->right_ = node){
-						newnode->parent_->right_ = newnode;
+
+
+					if (node != root_) {
+
+						if (node->parent_->right_ = node) {
+							newnode->parent_->right_ = newnode;
+						}
+						else {
+							newnode->parent_->left_ = newnode;
+						}
 					}
-					else{
-						newnode->parent_->left_ = newnode;
+					else {
+						root_ = newnode;
 					}
+					
 
 				}
-				else 
+				else
 				{	//node->right_ || node->left_   
 
-					if (node->right_){
-						node->parent_->right_ = node->right_;
+					if (node->right_) {
+						node->parent_->left_ = node->right_;
 					}
-					if (node->left_){
-						node->parent_->left_ = node->left_;
+					if (node->left_) {
+						node->parent_->right_ = node->left_;
 					}
 
 				}
-					node=nullptr;
+				node = nullptr;
+				
 			}
 		}
 	}
@@ -297,14 +321,7 @@ public:
 		}
 		return node;
 	}
-	std::shared_ptr<Node> Rightmost(std::shared_ptr<Node> node) {
-		if (node == nullptr)
-			return nullptr;
-		if (node->right_ != nullptr) {
-			return Rightmost(node->right_);
-		}
-		return node;
-	}
+ 
 
 
 
@@ -321,11 +338,10 @@ public:
 		if (!node)
 			return;
 
-		symmetric_walk(out,node->left_);
+		symmetric_walk(out, node->left_);
 		out << node->value_ << "  ";
-		symmetric_walk(out,node->right_);
+		symmetric_walk(out, node->right_);
 	}
 
 
 };
-
